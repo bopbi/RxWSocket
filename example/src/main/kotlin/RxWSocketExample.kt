09ede7ce1a541fcdad95598d9221ca6caba92b0a
Bobby Prabowo <bobby@kulinr.com>
2017-07-16 01:12:04 +0700

    add example

--- example/src/main/kotlin/RxWSocketExample.kt
+++ example/src/main/kotlin/RxWSocketExample.kt
@@ -1,3 +1,4 @@
+import okhttp3.OkHttpClient
 import okhttp3.Request
 
 /**
@@ -8,8 +9,33 @@ fun main(args : Array<String>) {
 
     val request = Request.Builder()
             .get()
-            .url("ws://10.10.0.2:8080/ws")
+            .url("ws://echo.websocket.org/") // use echo websocket test server
             .build()
 
-    RxWSocket()
+    RxWSocket(OkHttpClient(), request).webSocketFlowable().subscribe {
+        if (it is RxWSOpenEvent) {
+            println("Opened")
+            it.webSocket?.send("Send")
+        }
+
+        if (it is RxWSMessageStringEvent) {
+            println("Message String: " + it.text)
+        }
+
+        if (it is RxWSMessageByteEvent) {
+            print("Message Byte: " + it.bytes)
+        }
+
+        if (it is RxWSClosingEvent) {
+            print("Closing")
+        }
+
+        if (it is RxWSFailureEvent) {
+            print("Failure")
+        }
+
+        if (it is RxWSClosedEvent) {
+            print("Closed")
+        }
+    }
 }
